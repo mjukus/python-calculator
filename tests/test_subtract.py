d06858658632a61fe1b20731c7c5777deefcea17
Aaron Fletcher <afletcher53@gmail.com>
2023-11-13 12:26:18 +0000

    Add subtract function to pythoncalculator package and create test for it
    - Added a new file `subtract.py` to the `pythoncalculator` package, which contains the implementation of the `subtract` function.
    - Deleted the old `subtract.py` file.
    - Created a new test file `test_subtract.py` in the `tests` directory, which tests the correctness of the `subtract` function.

---
+++ tests/test_subtract.py
@@ -0,0 +1,7 @@
+"""Test the subtract function from the pythoncalculator package"""
+from pythoncalculator import subtract
+
+
+def test_subtract():
+    """Test that subtract returns correct result"""
+    assert subtract(1, 3) == -1
